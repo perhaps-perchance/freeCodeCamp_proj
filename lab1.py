2661c661966c007a2fae644d4f75e5d5539c627b
perhaps-perchance <262800723+perhaps-perchance@users.noreply.github.com>
2026-02-20 12:32:20 +0000

    Create lab1.py
    added my first lab demo file

---
+++ lab1.py
@@ -0,0 +1,7 @@
+encrypted_text = caesar('freeCodeCamp', 3')
+def caesar(text, shift):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet, shifted_alphabet)
+    encrypted_text = text.translate(translation_table)
+    print(encrypted_text)
